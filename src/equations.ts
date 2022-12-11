bd71e685967a83578ba181357375a3a58b70665d
Tool Cool <toolcool.org@gmail.com>
2022-12-12 01:10:36 +0200

    System of equations - the general case

--- src/equations.ts
+++ src/equations.ts
@@ -1,9 +1,12 @@
-import { Matrix2, Vector2, Vector3 } from './types';
-import { m2Inverse, mMulVector } from './matrix';
+import { Matrix, Matrix2, Matrix3, Vector, Vector2, Vector3 } from './types';
+import { m2Inverse, m3Inverse, mInverse, mMulVector } from './matrix';
 
 /**
- * System of equation of the second degree.
+ * System of equations of the second degree.
  * [x, y] = inverse(Matrix of equation parameters) x (vector of equation results)
+ * ---------------
+ * 3x + 2y = 7
+ * -6x + 6y = 6
  */
 export const equationSystem2 = (equation1: Vector3, equation2: Vector3, decimalPlaces = Infinity) : Vector2 | null => {
     const equationParams: Matrix2 = [
@@ -20,4 +23,65 @@ export const equationSystem2 = (equation1: Vector3, equation2: Vector3, decimalP
     ];
 
     return mMulVector(inversed, equationResults, decimalPlaces) as Vector2;
+};
+
+/**
+ * System of equations of the third degree.
+ * ---------------------------------------
+ * 3x + 2y + 5z = 7
+ * -6x + 6y + 6z = 6
+ * 2x + 7y - z = 4
+ */
+export const equationSystem3 = (
+    equation1: Vector3,
+    equation2: Vector3,
+    equation3: Vector3,
+    decimalPlaces = Infinity) : Vector3 | null => {
+    const equationParams: Matrix3 = [
+        [equation1[0], equation1[1], equation1[2]],
+        [equation2[0], equation2[1], equation2[2]],
+        [equation3[0], equation3[1], equation3[2]],
+    ];
+
+    const inversed = m3Inverse(equationParams);
+    if(inversed === null) return null; // no results
+
+    const equationResults: Vector3 = [
+        equation1[2],
+        equation2[2],
+        equation3[2]
+    ];
+
+    return mMulVector(inversed, equationResults, decimalPlaces) as Vector3;
+};
+
+/**
+ * System of equations of the Nth degree.
+ */
+export const equationSystemN = (equations: Matrix, decimalPlaces = Infinity) : Vector | null => {
+    if(equations.length <= 0) return null;
+
+    const equationParams: Matrix = [];
+
+    // fill params matrix - it contains all values from equations matrix except the last column,
+    // as the last columns is the results
+    for(let i=0; i<equations.length; i++){
+        const vector: Vector = [];
+        for(let j=0; j<equations[i].length - 1; i++){
+            vector.push(equations[i][j]);
+        }
+        equationParams.push(vector);
+    }
+
+    const inversed = mInverse(equationParams);
+    if(inversed === null) return null; // no results
+
+    // the last column of the equations matrix
+    const equationResults: Vector = [];
+    for(let i=0; i<equations.length; i++){
+        const row = equations[i];
+        equationResults.push(row[row.length - 1]);
+    }
+
+    return mMulVector(inversed, equationResults, decimalPlaces) as Vector;
 };
--- types/equations.d.ts
+++ types/equations.d.ts
@@ -1,6 +1,21 @@
-import { Vector2, Vector3 } from './types';
+import { Matrix, Vector, Vector2, Vector3 } from './types';
 /**
  * System of equation of the second degree.
  * [x, y] = inverse(Matrix of equation parameters) x (vector of equation results)
+ * ---------------
+ * 3x + 2y = 7
+ * -6x + 6y = 6
  */
 export declare const equationSystem2: (equation1: Vector3, equation2: Vector3, decimalPlaces?: number) => Vector2 | null;
+/**
+ * System of equation of the third degree.
+ * ---------------------------------------
+ * 3x + 2y + 5z = 7
+ * -6x + 6y + 6z = 6
+ * 2x + 7y - z = 4
+ */
+export declare const equationSystem3: (equation1: Vector3, equation2: Vector3, equation3: Vector3, decimalPlaces?: number) => Vector3 | null;
+/**
+ * System of equation of the Nth degree.
+ */
+export declare const equationSystemN: (equations: Matrix, decimalPlaces?: number) => Vector | null;
